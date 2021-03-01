8f302559ad88bf4b95fec1a13f0ce4e7e47a630b
NGTmeaty <NGTmeaty@users.noreply.github.com>
2021-03-01 01:30:09 -0500

    Add latest Wget-AT version as well as support for new warrior

--- pipeline.py
+++ pipeline.py
@@ -37,10 +37,12 @@ if StrictVersion(seesaw.__version__) < StrictVersion('0.8.5'):
 WGET_AT = find_executable(
     'Wget+AT',
     [
-        'GNU Wget 1.20.3-at.20200919.01',
-        'GNU Wget 1.20.3-at.20201030.01'
+        'GNU Wget 1.20.3-at.20210212.02'
     ],
-    ['./wget-at']
+    [
+        './wget-at', 
+        '/home/warrior/data/wget-at'
+    ]
 )
 
 if not WGET_AT:
